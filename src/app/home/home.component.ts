1d0098a22b31ae698964af313503c3c519c9d777
Diego Santana <diegosantana@MacBook-Air-de-Diego.local>
2023-03-12 21:47:03 -0300

    Criacao de página de home

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+import { ChartService } from "src/acl/service/chart.service";
+import { ChartDto } from "src/models/chartDto";
+import { Interval } from "src/models/interval.enum";
+import { Range } from "src/models/range.enum";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
+})
+export class HomeComponent {
+  chartdata: any;
+  constructor(private chartService: ChartService) {}
+
+  public ngOnInit() {
+    this.chartService
+      .getChartData(`PETR4.SA`, Interval.day, Range.month)
+      .subscribe((res) => (this.chartdata = res));
+  }
+}
